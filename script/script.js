5e6c61d2a10c986390a247fb138f006dae26ae1b
chrisopenclass <90398020+chrisopenclass@users.noreply.github.com>
2022-02-08 13:46:58 +0100

    ajout des fichiers

---
+++ script/script.js
@@ -0,0 +1,130 @@
+//url général de l'api
+const url_general_api = "http://localhost:8000/api/v1/titles/";
+
+// url pour avoir le meilleur film 
+const url_meilleur_film = "http://localhost:8000/api/v1/titles/?page_size=1&sort_by=-imdb_score";
+
+const url_sept_meilleur_film = "http://localhost:8000/api/v1/titles/?page_size=7&sort_by=-imdb_score";
+const url_sept_meilleur_action = "http://localhost:8000/api/v1/titles/?page_size=7&sort_by=-imdb_score&genre=action";
+const url_sept_meilleur_romance = "http://localhost:8000/api/v1/titles/?page_size=7&sort_by=-imdb_score&genre=romance";
+const url_meilleur_sci_fi = "http://localhost:8000/api/v1/titles/?page_size=7&sort_by=-imdb_score&genre=Sci-Fi";
+
+function create_div(className) {
+    let div = document.createElement('div')
+    div.setAttribute('class', className)
+    return div;
+ };
+
+function construction_modal(url) {
+    fetch(url)
+        .then(function(res) {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .then(function(film) {
+            document.getElementById("modal").style.display = "block";
+            document.getElementById("img_modal").src = film.image_url
+            document.getElementById("titre").innerHTML = film.title
+            document.getElementById("genre").innerHTML = "genre : " + film.genres
+            document.getElementById("date").innerHTML = "date de sorti : " + film.date_published
+            document.getElementById("note").innerHTML = "note : " + film.votes + " points"
+            document.getElementById("score").innerHTML = "score imdb : " + film.imdb_score
+            document.getElementById("real").innerHTML = "realisateur : " + film.writers
+            document.getElementById("acteurs").innerHTML = "acteurs : " + film.actors
+            document.getElementById("dure").innerHTML = "durée : " + film.duration + " minutes"
+            document.getElementById("pays").innerHTML = "pay de création : " + film.countries
+            document.getElementById("boxoffice").innerHTML = " box office : " + film.worldwide_gross_income + " $"
+            document.getElementById("resumer").innerHTML = "resumer : " + film.long_description
+            let span = document.getElementsByClassName("close")[0];
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+        })
+};
+
+function meilleur_film(url) {
+    fetch(url)
+        .then(function(res) {
+            if (res.ok) {
+                return res.json();
+             }
+         })
+         .then(function(data) {
+            let meilleur_film = data.results[0]
+            let url = meilleur_film.url
+            let img = meilleur_film.image_url
+            let titre = meilleur_film.title
+            let img_tag = document.getElementById("best_film")
+            let meilleur_titre = document.getElementById("best_moovie_title")
+            meilleur_titre.innerHTML = titre
+            img_tag.src = img
+            img_tag.addEventListener("click", function() {
+                construction_modal(url)
+            })
+        })
+};
+
+function main() {
+    meilleur_film(url_meilleur_film);
+    carrousel(url_sept_meilleur_film, "carousel1");
+    carrousel(url_sept_meilleur_action, "carousel2");
+    carrousel(url_sept_meilleur_romance, "carousel3");
+    carrousel(url_meilleur_sci_fi, "carousel4");
+ }
+
+function carrousel(api_url, id) {
+     fetch(api_url)
+         .then(function(res) {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+         .then(function(data) {
+            let result = data.results;
+            let ratio = result.length / 3;
+            let caroussel = document.getElementById(id);
+            let parent = caroussel.parentNode
+            caroussel.style.width = (ratio * 100) + "%";
+            let next_button = create_div("next")
+            let prev_button = create_div("prev")
+            parent.appendChild(next_button)
+            parent.appendChild(prev_button)
+            result.forEach(elements => {
+                let url = elements.url
+                let item = create_div("item")
+                item.innerHTML = "<img src='" + elements.image_url + "'>";
+                item.style.width = (100 / 4 / ratio) + "%"
+                caroussel.appendChild(item)
+                item.addEventListener("click", function() {
+                    construction_modal(url)
+                })
+            })
+            var current_item = 0
+            var item_to_scroll = 1
+            next_button.addEventListener('click',function(){
+                if (current_item < 4 ){
+                    let i = current_item + item_to_scroll
+                let translateX = i * -100 / result.length
+                console.log(translateX)
+                caroussel.style.transform = 'translate3d(' + translateX+'%, 0, 0)'
+                current_item = i
+                }
+            })
+            prev_button.addEventListener('click',function(){
+                if (current_item > 0){
+                    let i = current_item - item_to_scroll
+                let translateX = i * -100 / result.length
+                caroussel.style.transform = 'translate3d(' + translateX+'%, 0, 0)'
+                current_item = i
+                }
+            })
+        })
+};
+
+main()

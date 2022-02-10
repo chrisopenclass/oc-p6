acc59f3d68d65fb3f1efc7fa70b0d001db51582b
chrisopenclass <90398020+chrisopenclass@users.noreply.github.com>
2022-02-10 02:13:58 +0100

    carousel infini + ajout de commentaire

--- script/script.js
+++ script/script.js
@@ -8,13 +8,13 @@ const url_sept_meilleur_film = "http://localhost:8000/api/v1/titles/?page_size=7
 const url_sept_meilleur_action = "http://localhost:8000/api/v1/titles/?page_size=7&sort_by=-imdb_score&genre=action";
 const url_sept_meilleur_romance = "http://localhost:8000/api/v1/titles/?page_size=7&sort_by=-imdb_score&genre=romance";
 const url_meilleur_sci_fi = "http://localhost:8000/api/v1/titles/?page_size=7&sort_by=-imdb_score&genre=Sci-Fi";
-
+// petite finction permettant de créer une div avec un nom de class
 function create_div(className) {
     let div = document.createElement('div')
     div.setAttribute('class', className)
     return div;
  };
-
+// creation de la fenétre modal
 function construction_modal(url) {
     fetch(url)
         .then(function(res) {
@@ -47,7 +47,7 @@ function construction_modal(url) {
             }
         })
 };
-
+//partie qui permet de récupérer les information du meilleur film de les ajouter  et de créer son modal
 function meilleur_film(url) {
     fetch(url)
         .then(function(res) {
@@ -69,7 +69,7 @@ function meilleur_film(url) {
             })
         })
 };
-
+//lancement de la fonction permettant de créer les carouselle pour chaque chaque cathégorie
 function main() {
     meilleur_film(url_meilleur_film);
     carrousel(url_sept_meilleur_film, "carousel1");
@@ -77,7 +77,7 @@ function main() {
     carrousel(url_sept_meilleur_romance, "carousel3");
     carrousel(url_meilleur_sci_fi, "carousel4");
  }
-
+// fonction créant un carousell pour chaque films des cathérie selectionner 
 function carrousel(api_url, id) {
      fetch(api_url)
          .then(function(res) {
@@ -95,34 +95,41 @@ function carrousel(api_url, id) {
             let prev_button = create_div("prev")
             parent.appendChild(next_button)
             parent.appendChild(prev_button)
+            // pour chaque film  on crée une div dans laquel on ajout un élément image 
             result.forEach(elements => {
                 let url = elements.url
                 let item = create_div("item")
+                //on ajoute le lien de l'image dans l'élément on 
                 item.innerHTML = "<img src='" + elements.image_url + "'>";
                 item.style.width = (100 / 4 / ratio) + "%"
                 caroussel.appendChild(item)
+                // pour chaque film on lance la fonction de création du modal le lien de la page du film en paramétre
                 item.addEventListener("click", function() {
                     construction_modal(url)
                 })
             })
             var current_item = 0
             var item_to_scroll = 1
+            //bouton du carousel permettant de scroll les items
             next_button.addEventListener('click',function(){
-                if (current_item < 4 ){
-                    let i = current_item + item_to_scroll
+                // condition qui vérifie ou on en est dans le carousel si on est à la fin alors on retourne au début
+                if (current_item > (result.length - ratio) ){
+                    current_item = -1
+                }
+                let i = current_item + item_to_scroll
                 let translateX = i * -100 / result.length
-                console.log(translateX)
                 caroussel.style.transform = 'translate3d(' + translateX+'%, 0, 0)'
                 current_item = i
-                }
             })
             prev_button.addEventListener('click',function(){
-                if (current_item > 0){
-                    let i = current_item - item_to_scroll
+                // condition qui vérifie ou on en est dans le carousel si on est au début alors on va à la fin
+                if (current_item <= 0 ){
+                    current_item = parseInt(result.length - ratio) 
+                }
+                let i = current_item - item_to_scroll
                 let translateX = i * -100 / result.length
                 caroussel.style.transform = 'translate3d(' + translateX+'%, 0, 0)'
                 current_item = i
-                }
             })
         })
 };
